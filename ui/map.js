95413fcc3a66af2798dbd37b20c064717dd751ba
Marcin N-K (buttitch) <marcin.nk@gmail.com>
2013-06-02 15:27:10 +0930

    Fixed map style issues

--- ui/map.js
+++ ui/map.js
@@ -1,93 +1,117 @@
 //---
-// Map
+//Map
 //---
 
-var map = new OpenLayers.Map({
-	controls : [ new OpenLayers.Control.PanPanel(),
-			new OpenLayers.Control.ZoomPanel(),
-			new OpenLayers.Control.ScaleLine(),
-			new OpenLayers.Control.Attribution(),
-			new OpenLayers.Control.KeyboardDefaults() ]
-});
+Ext.onReady(function() {
 
-//var blueMarble = new OpenLayers.Layer.WMS("Blue Marble - Global Imagery",
-//		"http://maps.opengeo.org/geowebcache/service/wms", {
-//			layers : "bluemarble", 
-//			transparent: true
-//		}, {
-//			wrapDateLine : true,
-//			transitionEffect : 'resize',
-//			opacity: 0.2
-//		});
+	var pureCoverage = false;
+    //	pink tile avoidance
+	OpenLayers.IMAGE_RELOAD_ATTEMPTS = 5;
+    //	make OL compute scale according to WMS spec
+	OpenLayers.DOTS_PER_INCH = 25.4 / 0.28;
 
-var openStreetMap = new OpenLayers.Layer.WMS( "OSGEO Base Map",
-		"http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'}, {wrapDateLine: true, transitionEffect: 'resize'} );
+	var format = 'image/png';
 
-var pop2010HeatMap = new OpenLayers.Layer.WMS(
-		"2010 Population Heat Map",
-		"http://evolvesa.org:8080/geoserver/govhack/wms?service=WMS&version=1.1.0",
-		{
-			layers : 'govhack:lga_pop_2010_heatmap',
-			transparent : true,
-			format : "image/png"
-		}, {
-			isBaseLayer : false
-			//,
-			//wrapDateLine : true
-		});
+	var bounds = new OpenLayers.Bounds(
+			133.212999701183, -38.0626948581456,
+			140.973354941166, -28.9710283436566
+	);
+	var options = {
+			controls : [ new OpenLayers.Control.PanPanel(),
+			             new OpenLayers.Control.ZoomPanel(),
+			             new OpenLayers.Control.ScaleLine(),
+			             new OpenLayers.Control.Attribution(),
+			             new OpenLayers.Control.KeyboardDefaults() ],
+//			maxExtent: bounds,
+//			maxResolution: 0.0355143223222226,
+			projection: "EPSG:4326",
+			units: 'degrees'
+	};
+	var map = new OpenLayers.Map(options);
 
 
-map.addLayer(openStreetMap);
-//map.addLayer(blueMarble);
-map.addLayer(pop2010HeatMap);
+//	var blueMarble = new OpenLayers.Layer.WMS("Blue Marble - Global Imagery",
+//	"http://maps.opengeo.org/geowebcache/service/wms", {
+//	layers : "bluemarble", 
+//	transparent: true
+//	}, {
+//	wrapDateLine : true,
+//	transitionEffect : 'resize',
+//	opacity: 0.2
+//	});
 
-// ---
-// Controls
-// ---
+	var openStreetMap = new OpenLayers.Layer.WMS( "OSGEO Base Map",
+			"http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'}, {wrapDateLine: true, transitionEffect: 'resize'} );
 
-var zoomSlider = new GeoExt.ZoomSlider({
-	map : map,
-	aggressive : true,
-	vertical : true,
-	height : 100,
-	plugins : new GeoExt.ZoomSliderTip({
-		template : "<div>Zoom Level: {zoom}</div><div>Scale: 1 : {scale}</div>"
-	})
-});
-
-var panZoom = new GeoExt.Action({
-	tooltip : "Pan and zoom around the map",
-	iconCls : "gxp-icon-pan",
-	toggleGroup : "navigation",
-	pressed : true,
-	allowDepress : false,
-	control : new OpenLayers.Control.Navigation(),
-	map : map,
-	handler : function() {
-		Ext.getCmp("map").body.applyStyles("cursor:default");
-		var element = document.getElementById("output");
-		element.innerHTML = "";
-		layerRuler.removeFeatures(layerRuler.features);
-	}
-});
-var contents$ = $('#map-container');
-var mapPanel = new GeoExt.MapPanel({
-	map : map,
-	collapsible : false,
-	frame : false,
-	items : [ zoomSlider ],
-	width : contents$.width(),
-	height : contents$.height(),
-	renderTo : "map-inner"
-});
-
-// set map
-map.setCenter(new OpenLayers.LonLat(138, -34));
-map.zoomTo(3);
+	var pop2010HeatMap = new OpenLayers.Layer.WMS(
+			"2010 Population Heat Map",
+			"http://evolvesa.org:8080/geoserver/govhack/wms?service=WMS&version=1.1.0",
+			{
+				LAYERS: 'govhack:lga_pop_2010_heatmap',
+				STYLES: '',
+				format: format,
+				transparent: true
+			},
+			{
+				buffer: 1,
+				isBaseLayer: false,
+				yx : {'EPSG:4326' : true}, 
+				transitionEffect: 'resize',
+				singleTile: true
+			} );
 
 
+	map.addLayer(openStreetMap);
+//	map.addLayer(blueMarble);
+	map.addLayer(pop2010HeatMap);
 
-// handle window resize events
-$(window).resize(function() {
-	mapPanel.setSize(contents$.width(), contents$.height());
+	//	---
+	//	Controls
+	//	---
+
+	var zoomSlider = new GeoExt.ZoomSlider({
+		map : map,
+		aggressive : true,
+		vertical : true,
+		height : 100,
+		plugins : new GeoExt.ZoomSliderTip({
+			template : "<div>Zoom Level: {zoom}</div><div>Scale: 1 : {scale}</div>"
+		})
+	});
+
+	var panZoom = new GeoExt.Action({
+		tooltip : "Pan and zoom around the map",
+		iconCls : "gxp-icon-pan",
+		toggleGroup : "navigation",
+		pressed : true,
+		allowDepress : false,
+		control : new OpenLayers.Control.Navigation(),
+		map : map,
+		handler : function() {
+			Ext.getCmp("map").body.applyStyles("cursor:default");
+			var element = document.getElementById("output");
+			element.innerHTML = "";
+			layerRuler.removeFeatures(layerRuler.features);
+		}
+	});
+	var contents$ = $('#map-container');
+	var mapPanel = new GeoExt.MapPanel({
+		map : map,
+		collapsible : false,
+		frame : false,
+		items : [ zoomSlider ],
+		width : contents$.width(),
+		height : contents$.height(),
+		renderTo : "map-inner"
+	});
+
+	//	set map
+	map.setCenter(new OpenLayers.LonLat(138, -34));
+	map.zoomTo(6);
+
+	//	handle window resize events
+	$(window).resize(function() {
+		mapPanel.setSize(contents$.width(), contents$.height());
+	});
+	
 });
